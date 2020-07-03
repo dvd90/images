99de6ed57d4f1469a167c191fc9466b4595cdedc
David Sellam <dvdsellam@gmail.com>
2020-07-03 12:28:36 +0300

    app images uploading setup

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <div class="ui secondary pointing menu">
+    <a class="active item">Home</a>
+    <a class="item">Messages</a>
+    <a class="item">Friends</a>
+    <div class="right menu">
+      <a class="ui item">Logout</a>
+    </div>
+  </div>
+  <div class="ui segment">
+    <p></p>
+  </div>-->
+  <div class="ui secondary pointing menu">
+    <a class="active item" href="/">Image Storage</a>
+    <div class="right menu">
+      <a class="ui item">Log In</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  methods: {}
+};
+</script>
